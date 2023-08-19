2932e4a54c88f7d031dc697df9591501b1b065d2
Seven Star <sevenstar@Sevens-MacBook-Air.local>
2023-08-19 10:57:51 -0700

    dataConnection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Axios  from 'axios'
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+function App(){
+  const [data, setData] =useState("");
+  const getdata = () => {
+  Axios.get('https://official-joke-api.appspot.com/random_joke').then(
+    (response) => {
+      setData(response.data.setup)
+    }
   );
-}
 
-export default App;
+};
+
+  return (
+    <div>
+      Data connection:
+      <button onClick={getdata}>click here to get data</button>
+      {data}
+    </div>
+  )
+
+  }
+export default App
